82285607ffc5756bdcae3c8f2467fb46f0daa9ba
zhouws <584188322@qq.com>
2022-03-08 22:31:23 +0800

    新增Person,添加age和name属性

---
+++ src/com/msb/git01/Person.java
@@ -0,0 +1,12 @@
+package com.msb.git01;
+
+/**
+ * @Auther: zhouws
+ * @Date: 2022/03/08 03-08 22:30
+ * @Descripition: com.msb.git01
+ * @version: 1.0
+ */
+public class Person {
+    private int age;
+    private String name;
+}
