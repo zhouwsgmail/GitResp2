96e4284c0714d74a480a3e69d535d1bc3a7f76e1
zhouws <584188322@qq.com>
2022-03-08 22:17:26 +0800

    第一次使用idea提交git

---
+++ src/com/msb/git01/HelloWord.java
@@ -0,0 +1,13 @@
+package com.msb.git01;
+
+/**
+ * @Auther: zhouws
+ * @Date: 2022/03/08 03-08 22:13
+ * @Descripition: com.msb.git01
+ * @version: 1.0
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("HelloWord");
+    }
+}
